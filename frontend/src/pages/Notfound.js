70b0d5853f6a884905f0c3acf65bce53d4636f88
arpitgulhane8 <162834636+arpitgulhane8@users.noreply.github.com>
2024-11-04 12:23:28 +0530

    Update Notfound.js

--- frontend/src/pages/Notfound.js
+++ frontend/src/pages/Notfound.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import "../styles/notfound.css";
 
 function Notfound() {
   return (
-    <div>
-      NotFound
+    <div className="not-found-container">
+      <h1 className="not-found-title">
+        <i className="fas fa-exclamation-triangle"></i> 404
+      </h1>
+      <i className="fas fa-sad-cry"></i>
+      <p className="not-found-text">
+        Sorry, the page you're looking for cannot be found.
+      </p>
+      <a href="/" className="home-link">
+        Go back to Home
+      </a>
     </div>
   )
 }
